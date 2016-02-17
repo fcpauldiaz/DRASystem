5fa71d65999fd03fbf1e73d40546af7e154e63ee
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2016-02-17 13:59:40 -0600

    heroku config

---
+++ app/config/production_parameters.php
@@ -0,0 +1,14 @@
+<?php
+
+      $container->setParameter('database_driver', 'pdo_mysql');
+    $container->setParameter('database_host', getenv('MYSQL_HOST'));
+    $container->setParameter('database_port', 3306);
+    $container->setParameter('database_name', getenv('MYSQL_NAME'));
+    $container->setParameter('database_user', 'fcpauldiaz');
+    $container->setParameter('database_password', getenv('MYSQL_PASSWORD'));
+    $container->setParameter('secret', getenv('SECRET'));
+    $container->setParameter('locale', 'es');
+    $container->setParameter('mailer_transport', gmail);
+    $container->setParameter('mailer_host', null);
+    $container->setParameter('mailer_user', getenv('EMAIL_USER'));
+    $container->setParameter('mailer_password', getenv('EMAIL_PASSWORD'));
