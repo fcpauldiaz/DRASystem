4914c6b620ae632cd12a3f2f2dbd917c6d01c345
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2016-07-17 12:39:36 -0600

    refactor click checkbox logic

--- web/js/tipo_puesto_click.js
+++ web/js/tipo_puesto_click.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	
-	function disableCheckBox(param) {
+	function disableCheckBox(param, parent) {
 		var checkboxSelected = $('#userbundle_tipopuesto_permisos_'+param);
-		var checked = checkboxSelected[0].checked;
+		var checked = parent[0].checked;
 		if (checked === true) {
 			checkboxSelected.iCheck('uncheck');
 			checkboxSelected.iCheck('enable');
@@ -15,139 +15,139 @@ $(document).ready(function(){
 		//se tiene que manejar un chedkbox para cada permiso
 	//permiso para crear clientes
 	$('#userbundle_tipopuesto_permisos_10').on('ifClicked', function(event){
-		disableCheckBox('8');
+		disableCheckBox('8',$(this));
 	});
       
     //se tiene que manejar un chedkbox para cada permiso
 	//permiso para crear actividades
 	$('#userbundle_tipopuesto_permisos_11').on('ifClicked', function(event){
-		disableCheckBox('3');
+		disableCheckBox('3',$(this));
 	});
 
 	//se tiene que manejar un chedkbox para cada permiso
 	//permiso para crear horas invertidas
 	$('#userbundle_tipopuesto_permisos_12').on('ifClicked', function(event){
-		disableCheckBox('2');
+		disableCheckBox('2',$(this));
     }); 
     //se tiene que manejar un chedkbox para cada permiso
 	//permiso para crear presupuestos
 	$('#userbundle_tipopuesto_permisos_13').on('ifClicked', function(event){
-		disableCheckBox('4');
+		disableCheckBox('4',$(this));
      }); 
     //se tiene que manejar un chedkbox para cada permiso
 	//permiso para crear tipos de puesto
 	$('#userbundle_tipopuesto_permisos_15').on('ifClicked', function(event){
-		disableCheckBox('6')
+		disableCheckBox('6',$(this))
       }); 
      //se tiene que manejar un chedkbox para cada permiso
 	//permiso para crear departamentos
 	$('#userbundle_tipopuesto_permisos_16').on('ifClicked', function(event){
-		disableCheckBox('7');
+		disableCheckBox('7',$(this));
       }); 
     //se tiene que manejar un chedkbox para cada permiso
 	//permiso para editar horas invertidas
 	$('#userbundle_tipopuesto_permisos_17').on('ifClicked', function(event){
-		disableCheckBox('12');
-		disableCheckBox('2');
+		disableCheckBox('12',$(this));
+		disableCheckBox('2',$(this));
 	});
 	//permiso para editar actividades
 	$('#userbundle_tipopuesto_permisos_18').on('ifClicked', function(event){
-		disableCheckBox('11');
-		disableCheckBox('3');
+		disableCheckBox('11',$(this));
+		disableCheckBox('3',$(this));
 	});
 	
 	//permiso para editar presupuestos
 	$('#userbundle_tipopuesto_permisos_19').on('ifClicked', function(event){
-		disableCheckBox('13');
-		disableCheckBox('4');
+		disableCheckBox('13',$(this));
+		disableCheckBox('4',$(this));
     }); 
 	
     //permiso para editar clientes
     $('#userbundle_tipopuesto_permisos_20').on('ifClicked', function(event){
-		disableCheckBox('10');
-		disableCheckBox('8');
+		disableCheckBox('10',$(this));
+		disableCheckBox('8',$(this));
     });
     //permiso para editar usuarios
     $('#userbundle_tipopuesto_permisos_21').on('ifClicked', function(event){
-		disableCheckBox('14');
+		disableCheckBox('14',$(this));
     });
      //permiso para editar tipo puestos
     $('#userbundle_tipopuesto_permisos_22').on('ifClicked', function(event){
-		disableCheckBox('15');
-		disableCheckBox('6');
+		disableCheckBox('15',$(this));
+		disableCheckBox('6',$(this));
     });  
      //permiso para editar tipo departamentos
     $('#userbundle_tipopuesto_permisos_23').on('ifClicked', function(event){
-		disableCheckBox('16');
-		disableCheckBox('7');
+		disableCheckBox('16',$(this));
+		disableCheckBox('7',$(this));
     }); 
      //permiso para eliminar horas invertidas
     $('#userbundle_tipopuesto_permisos_24').on('ifClicked', function(event){
-		disableCheckBox('2');
-		disableCheckBox('12');
-		disableCheckBox('17');
+		disableCheckBox('2',$(this));
+		disableCheckBox('12',$(this));
+		disableCheckBox('17',$(this));
     });   
      //permiso para eliminar presupuestos
     $('#userbundle_tipopuesto_permisos_25').on('ifClicked', function(event){
-		disableCheckBox('4');
-		disableCheckBox('13');
-		disableCheckBox('19');
+		disableCheckBox('4',$(this));
+		disableCheckBox('13',$(this));
+		disableCheckBox('19',$(this));
     });
      //permiso para eliminar actividades
     $('#userbundle_tipopuesto_permisos_26').on('ifClicked', function(event){
-		disableCheckBox('3');
-		disableCheckBox('11');
-		disableCheckBox('18');
+		disableCheckBox('3',$(this));
+		disableCheckBox('11',$(this));
+		disableCheckBox('18',$(this));
     });
             
      //permiso para eliminar clientes
     $('#userbundle_tipopuesto_permisos_27').on('ifClicked', function(event){
-		disableCheckBox('8');
-		disableCheckBox('10');
-		disableCheckBox('20');
+		disableCheckBox('8',$(this));
+		disableCheckBox('10',$(this));
+		disableCheckBox('20',$(this));
     }); 
       //permiso para eliminar tipo puestos
     $('#userbundle_tipopuesto_permisos_28').on('ifClicked', function(event){
-		disableCheckBox('6');
-		disableCheckBox('15');
-		disableCheckBox('22');
+		disableCheckBox('6',$(this));
+		disableCheckBox('15',$(this));
+		disableCheckBox('22',$(this));
     });
        //permiso para eliminar departamentos
     $('#userbundle_tipopuesto_permisos_29').on('ifClicked', function(event){
-		disableCheckBox('7');
-		disableCheckBox('16');
-		disableCheckBox('23');
+		disableCheckBox('7',$(this));
+		disableCheckBox('16',$(this));
+		disableCheckBox('23',$(this));
     });
 	$('#userbundle_tipopuesto_permisos_30').on('ifClicked', function(event){
-		disableCheckBox('1');
-		disableCheckBox('2');
-		disableCheckBox('3');
-		disableCheckBox('4');
-		disableCheckBox('5');
-		disableCheckBox('6');
-		disableCheckBox('7');
-		disableCheckBox('8');
-		disableCheckBox('9');
-		disableCheckBox('10');
-		disableCheckBox('11');
-		disableCheckBox('12');
-		disableCheckBox('13');
-		disableCheckBox('14');
-		disableCheckBox('15');
-		disableCheckBox('16');
-		disableCheckBox('17');
-		disableCheckBox('18');
-		disableCheckBox('19');
-		disableCheckBox('20');
-		disableCheckBox('21');
-		disableCheckBox('22');
-		disableCheckBox('23');
-		disableCheckBox('24');
-		disableCheckBox('25');
-		disableCheckBox('26');
-		disableCheckBox('27');
-		disableCheckBox('28');
-		disableCheckBox('29');
+		disableCheckBox('1',$(this));
+		disableCheckBox('2',$(this));
+		disableCheckBox('3',$(this));
+		disableCheckBox('4',$(this));
+		disableCheckBox('5',$(this));
+		disableCheckBox('6',$(this));
+		disableCheckBox('7',$(this));
+		disableCheckBox('8',$(this));
+		disableCheckBox('9',$(this));
+		disableCheckBox('10',$(this));
+		disableCheckBox('11',$(this));
+		disableCheckBox('12',$(this));
+		disableCheckBox('13',$(this));
+		disableCheckBox('14',$(this));
+		disableCheckBox('15',$(this));
+		disableCheckBox('16',$(this));
+		disableCheckBox('17',$(this));
+		disableCheckBox('18',$(this));
+		disableCheckBox('19',$(this));
+		disableCheckBox('20',$(this));
+		disableCheckBox('21',$(this));
+		disableCheckBox('22',$(this));
+		disableCheckBox('23',$(this));
+		disableCheckBox('24',$(this));
+		disableCheckBox('25',$(this));
+		disableCheckBox('26',$(this));
+		disableCheckBox('27',$(this));
+		disableCheckBox('28',$(this));
+		disableCheckBox('29',$(this));
 	});
 
 })
