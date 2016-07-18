163b812a1681302f25d636af29f294eb9445534e
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2016-07-18 00:48:26 -0600

    fix date modifier.

--- web/js/dateModifier.js
+++ web/js/dateModifier.js
@@ -7,10 +7,10 @@ $('.fecha-inicial')
     }
     else {
        
-        var firstDay = new Date(date.getFullYear(), date.getMonth() + 2 , 1);
+        var firstDay = new Date(date.getFullYear(), date.getMonth() + 1, 1);
     }
     var day = firstDay.getDate();
-    var month = firstDay.getMonth() ;
+    var month = firstDay.getMonth()+1 ;
     var year = firstDay.getFullYear();
    
     if (day < 10) {
@@ -28,13 +28,14 @@ $('.fecha-final')
     .on('changeMonth', function(ev){
     date = ev.date;
      if (this.value == '' || verifier == true ){
-       var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+       var lastDay = new Date(date.getFullYear(), date.getMonth() + 2, 0);
     }
     else {
         var lastDay = new Date(date.getFullYear(), date.getMonth() + 2, 0);
     }
     var day = lastDay.getDate();
     var month = lastDay.getMonth()+1;
+   
     var year = lastDay.getFullYear();
    
      if (day < 10) {
