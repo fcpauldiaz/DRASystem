515052cc27ed3f43a8b5b29557f40f433053c910
Pablo Díaz <pablo5_diaz@hotmail.com>
2016-09-08 00:26:15 -0600

    move forms to type folder

---
+++ src/UserBundle/Form/Type/DescuentoType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DescuentoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombreDescuento')
+            ->add('monto')
+            ->add('prestaciones')
+            ->add('tipoDescuento')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\Descuento',
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'userbundle_descuento';
+    }
+}
