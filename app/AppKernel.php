35f1546bc8233340a0ec1759cc1c18109c5a8899
PABLO DIAZ <pablo5_diaz@hotmail.com>
2016-10-13 18:35:56 -0600

    add data tables dip

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,6 +39,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new Troopers\AlertifyBundle\TroopersAlertifyBundle(),
+            new Sg\DatatablesBundle\SgDatatablesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
