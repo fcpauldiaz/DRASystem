6d929bddcab7a555a38be9eb3e86dbc30c78283c
PABLO DIAZ <pablo5_diaz@hotmail.com>
2016-10-15 22:56:54 -0600

    add cron job options

--- src/CostoBundle/Controller/CronJobController.php
+++ src/CostoBundle/Controller/CronJobController.php
@@ -17,6 +17,8 @@ class CronJobController extends Controller
      */
     public function calcularCostoAction(Request $request)
     {
+        $form = $this->createFormAction();
+        $form->handleRequest($request);
         $em = $this->getDoctrine()->getManager();
 
         $usuarios = $em->getRepository('UserBundle:UsuarioTrabajador')->findAll();
@@ -24,9 +26,50 @@ class CronJobController extends Controller
         $firstDay = new \DateTime('first day of this month');
 
         $lastDay = new \Datetime('last day of this month');
+        if ($form->isValid()) {
+            $firstDayRequest = $form->getData()['fechaInicio'];
+            $lastDayRequest = $form->getData()['fechaFinal'];
+            $usuariosRequest = $form->getData()['usuarios'];
+
+            if ($firstDayRequest !== null) {
+                $firstDay  = $firstDayRequest;
+                if ($lastDayRequest === null) {
+                    $this->addFlash('error', 'No puede dejar solo un campo de fecha en blanco');
+                    return $this->render('CostoBundle:Costo:cronJob.html.twig',
+                        [
+                            'verificador' => true, //mandar variable a javascript
+                            'form' => $form->createView(),
+                        ]
+                    );
+                }
+            }
+            if ($lastDayRequest !== null) {
+                if ($firstDayRequest === null) {
+                    $this->addFlash('error', 'No puede dejar solo un campo de fecha en blanco');
+                    return $this->render('CostoBundle:Costo:cronJob.html.twig',
+                        [
+                            'verificador' => true, //mandar variable a javascript
+                            'form' => $form->createView(),
+                        ]
+                    );
+                }
+                $lastDay = $lastDayRequest;
+            }
+            if ($usuariosRequest !== null && empty($usuariosRequest->toArray()) === false) {
+                $usuarios = $usuariosRequest;
+            }
+        }
 
         foreach ($usuarios as $usuario) {
             $entidadCosto = new Costo();
+            $actualizarCosto = $em->getRepository('CostoBundle:Costo')->findOneBy([
+                'fechaInicio' => $firstDay,
+                'fechaFinal' => $lastDay,
+                'usuario' => $usuario
+            ]);
+            if ($actualizarCosto !== null) {
+                $entidadCosto = $actualizarCosto;
+            }
             $entidadCosto->setFechaInicio($firstDay);
             $entidadCosto->setFechaFinal($lastDay);
             $costo = $this->calcularCosto(
@@ -40,7 +83,7 @@ class CronJobController extends Controller
             $em->persist($entidadCosto);
         }
         $em->flush();
-
+        $this->addFlash('success', 'Se han guardado los costos');
         return $this->redirect($this->generateUrl('cron_job_view'));
     }
 
@@ -49,17 +92,91 @@ class CronJobController extends Controller
      */
     public function showButtonAction()
     {
-        $form = $this->createFormBuilder()
-            ->setAction($this->generateUrl('cron_job'))
-            ->add('submit', 'submit', array('label' => 'Calcular'))
-            ->getForm()
-        ;
+       $form = $this->createFormAction();
 
         return $this->render('CostoBundle:Costo:cronJob.html.twig',
             [
+                'verificador' => false,
                 'form' => $form->createView(),
             ]
         );
+    }
+
+    public function createFormAction()
+    {
+         $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('cron_job'))
+             ->add('usuarios', 'entity', [
+                'class' => 'UserBundle:Usuario',
+                'required' => false,
+                'empty_value' => 'Seleccione el usuario',
+                 'attr' => [
+                    'class' => 'select2',
+                ],
+                'multiple' => true,
+                'label' => 'Usuarios (opcional)'
+
+            ])
+            ->add('fechaInicio', 'collot_datetime', ['pickerOptions' => [
+                'format' => 'dd/mm/yyyy',
+                'weekStart' => 0,
+                'autoclose' => true,
+                'startView' => 'year',
+                'minView' => 'year',
+                'maxView' => 'decade',
+                'todayBtn' => false,
+                'todayHighlight' => true,
+                'keyboardNavigation' => true,
+                'language' => 'es',
+                'forceParse' => false,
+                'minuteStep' => 5,
+                'pickerReferer ' => 'default', //deprecated
+                'pickerPosition' => 'bottom-right',
+                'viewSelect' => 'month',
+                'showMeridian' => false,
+            ],
+            'attr' => [
+                'class' => 'hide-element fecha-inicial',
+            ],
+            'label_attr' => [
+                'class' => 'hide-element',
+            ],
+            'label' => 'Fecha Inicial (opcional)',
+            'read_only' => true,
+            'required' => false,
+            ])
+           ->add('fechaFinal', 'collot_datetime', ['pickerOptions' => [
+                    'format' => 'dd/mm/yyyy',
+                    'weekStart' => 0,
+                    'autoclose' => true,
+                    'startView' => 'year',
+                    'minView' => 'year',
+                    'maxView' => 'decade',
+                    'todayBtn' => false,
+                    'todayHighlight' => true,
+                    'keyboardNavigation' => true,
+                    'language' => 'es',
+                    'forceParse' => false,
+                    'minuteStep' => 5,
+                    'pickerReferer ' => 'default', //deprecated
+                    'pickerPosition' => 'bottom-right',
+                    'viewSelect' => 'month',
+                    'showMeridian' => false,
+                ],
+
+                'attr' => [
+                    'class' => 'hide-element fecha-final',
+                ],
+                 'label' => 'Fecha Final (opcional)',
+                'label_attr' => [
+                    'class' => 'hide-element',
+                ],
+                'read_only' => true,
+                'required' => false,
+            ])
+            ->add('submit', 'submit', array('label' => 'Calcular'))
+            ->getForm();
+        return $form;
     }
 
     private function calcularCosto($fechaInicio, $fechaFin, $usuario)
