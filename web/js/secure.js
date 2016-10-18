da925523d1430dc117855ea7641a168e9807daa9
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2016-10-17 23:23:19 -0600

    remove logs

--- web/js/secure.js
+++ web/js/secure.js
@@ -1,20 +1,18 @@
   $(document).ready(function() {
-    console.log('ready');
+
     var key = localStorage.getItem('SessionId');
     var secret = localStorage.getItem('SessionKey');
 
     
     if (key !== null && secret !== null) {
-      console.log('sending ajax');
+
       var data = {'SessionId': key, 'SessionKey': secret};
-      console.log( window.location.origin+'/api/login')
-      console.log(data);
         $.ajax({
             url: window.location.origin+'/api/login',
             data: data,
             method: 'POST',
             success: function(data) {
-              console.log(data);
+
               if (data.valid === true){
                 notie.alert(1, 'Usuario recordado: iniciando sesión automáticamente');
                 setTimeout(function(){ }, 3000);
