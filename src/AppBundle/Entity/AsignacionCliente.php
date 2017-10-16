5a66d47c691303b7a5c1ee56df3431569997e681
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2017-10-16 10:27:18 -0600

    set null on client delete

--- src/AppBundle/Entity/AsignacionCliente.php
+++ src/AppBundle/Entity/AsignacionCliente.php
@@ -25,6 +25,7 @@ class AsignacionCliente
      * @var string
      *
      * @ORM\ManyToOne(targetEntity="Cliente", inversedBy="usuarioAsignados" ,cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=true, onDelete="SET NULL")
      */
     private $cliente;
 
