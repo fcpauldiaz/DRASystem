19411ddfdca18d31f4c346210406e39cb28f40e4
Pablo Díaz <pablo5_diaz@hotmail.com>
2020-05-22 12:21:06 -0600

    Add email production env variable

--- app/config/production_parameters.php
+++ app/config/production_parameters.php
@@ -9,6 +9,7 @@
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'es');
     $container->setParameter('api_email_key',getenv('API_EMAIL'));
+    $container->setParameter('api_email_domain', getenv('API_EMAIL_DOMAIN'))
     $container->setParameter('mailer_transport', 'gmail');
     $container->setParameter('mailer_host', null);
     $container->setParameter('mailer_user', getenv('EMAIL_USER'));
